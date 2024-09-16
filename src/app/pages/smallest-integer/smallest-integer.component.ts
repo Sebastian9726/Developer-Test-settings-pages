e407cbb7cc1b5409637d49a24b8085c15daba3f1
Sebastian <jstraslavina@fundaciongruposocial.co>
2024-09-15 19:38:31 -0500

    feat:Determine the smallest missing integer

--- src/app/pages/smallest-integer/smallest-integer.component.ts
+++ src/app/pages/smallest-integer/smallest-integer.component.ts
@@ -16,13 +16,20 @@ export class SmallestIntegerComponent implements OnInit {
         this.test([1, 6, 3, 4, 2, 1], 5); // should log 5
         this.test([1, 2, 3, 4, 5], 6); // should log 6
         this.test([-1, -6, 4], 1); // should log 1
+        
     }
 
     /**
      * Write your code inside this function
      */
     smallestMissingInt(A: number[]): number {
-        return 1;
+    
+            const positives = new Set(A.filter(x => x > 0));
+            let smallestMissing = 1;
+            while (positives.has(smallestMissing)) {
+                smallestMissing++;
+            }
+            return smallestMissing;
     }
 
     /**
